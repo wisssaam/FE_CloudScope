a3dcc56c6ceac27e04964a15afaf96c6b342322b
wissam.benboubaker <54308981+wisssaam@users.noreply.github.com>
2022-12-12 09:34:01 +0100

    Update fgs_download_fe_bills.py

--- fgs_download_fe_bills.py
+++ fgs_download_fe_bills.py
@@ -8,9 +8,7 @@ import sys
 
 C_cloudstore_base_url = "https://api.orange.com/cloud/b2b/v1"
 
-
 def upload_file_to_obs(obsAddr, bucket, objName, ak, sk):
-
     TestObs = ObsClient(access_key_id=ak, secret_access_key=sk,server=obsAddr)
     objAbsPath = os.path.join('/tmp', objName)   # Obtains the absolute path of a local file.
     resp = TestObs.putFile(bucketName=bucket, objectKey=objName, file_path=objAbsPath)
@@ -68,22 +66,11 @@ def computeCloudStoreHeader(cloudstore_credentials, contract_id, logger):
 def getCloudStoreDocumentsList(cloudstore_header, contract_id, document_type, max_files, logger): # 
     # build url to fetch the documents list
     url = "{}/documents".format(C_cloudstore_base_url)
-    #params = json.dumps({'documentType':document_type, 'limit':max_files})
-    #params = json.dumps({'documentType':"all", 'limit':100})            # sent only 10 documents : all the latest usage reports (type="reports")
-    #params = {'documentType': 'all', 'limit':10}                        # sent: {"documents": null}
-    #params = {'documentType': 'comsumptionRatedReports', 'limit': 8}    # sent the correct answer :)
-    #params = {'documentType': document_type, 'limit': max_files}        # sent the correct answer :) - with document_type='comsumptionRatedReports'
-    #params = {'documentType': document_type, 'limit': max_files}        # sent: {"documents": null} - with document_type='partialComsumptionRatedReports'
-    #params = {'documentType': 'toto', 'limit': max_files}               # sent: {"documents": null}
-    #params = {'documentType': '', 'limit': 100}                         # sent: the correct answer :)
 
     params = {'documentType': document_type, 'limit': max_files}        # sent: the correct answer :) - with document_type='partialConsumptionRatedReports'
     logger.info(params)
 
-
-
    # Get documents
-    #if True:
     try:
         r = requests.get(url, headers=cloudstore_header, params=params)
         
@@ -95,8 +82,6 @@ def getCloudStoreDocumentsList(cloudstore_header, contract_id, document_type, ma
         documents = r.json()
         logger.info("documents="+json.dumps(documents))
 
-
-    #if False:
     except:
         documents = None
     
@@ -163,12 +148,11 @@ def handler (event, context):
     global logger
 
     logger = context.getLogger()
-    document_type = 'bills'
-    max_files = 3
+
     contract_id = obs_address = context.getUserData('contract_id')
-    print(contract_id)
-    print(type(contract_id))
-    
+    document_type = obs_address = context.getUserData('document_type')
+    max_files = obs_address = int(context.getUserData('max_files'))
+
     cloudstore_credentials = """{
             "auth_header":"Basic WWczTFhSY0ZUR3c4dENIWnlYUHoyZTc3WERvN3R5M0k6UVRDSHNPdEwxRFBVMTNJUA==", 
             "api_key": "P+AuA5qT3UGLC+eQXgCZg/2DbPCjumzAA/P6ZhfBkAGMyV04pbnU/V3jfsj/Uj8FIw51QgtCbsXDEgVaf6nV+sh4nNcVyxHa8cImeUjHctrG1D11tTE/QSDaInPNA37slCx75WrMcIJ3CUj+DYPrUVON6zC+zUPBHC6TlzA8YKu1qzH5NwO+NIARWnPJ8vtBcs381AiOVby8lmFyGftz4hBKWblgGzDZ7HXK77WqyqrPA6Yi+dLMb3vScgXIM9BuaY5FJxBndFObEZt6qXx5XKkJodPBMGYHpJojRXYKXUuF6RDeG+r0nOv2bDstBHjw5Vx+snDr2KWbT6K1jHpiRvBzd/SAE5rzPRbV63RhO7J1+h1KxEhiccGEoA1A1Qd0hYTNcN05MKh3UcmpAzIwvogbmwnV5o4ZkHUAoeOpRVTzOkt3zGiaOmh5wHHP6z9IViQq0SnouwrTvRMyXNq7UCbN/ra0EXynlXyjG5OwbShMIAhGlglivBe+YRhPtTpRhYUo7n84WjOvIsQU2WZbDGJ8E0h1xLAIJIP1NUUMhSfcmPg21L1MVWAYXxmi2ctiGxBMFXCj/L8JIeL8z826xc4p1kpE9bfS0P/fQHK9olZCdgerwoADKsKQoeT+lLdI47OCNdazvvXD3SMagwHcP0ikyiHsF7xFALPGIfNO54uXtg2pgo51fPehixHMNpKARL2GVxq6kh1td5IfCV8v7++tQ4ygvUuRIqw1IWy6cEjK/dl6aWghBsMthT1ylYkY+RYvKQj0shWf0mYAMkElZr+JF8ZzG9RbicjZktb2sS+V4ipuCTNVlcMoOLKnib0z20J8HIo/P2yxstRE9Adyki/XHWjq7JRa7/kj0mwwhLTUAvFpHn1D0OsgYl+1gjHVhbFABwesL0kjW81/39XnXABChCNKu8F/7zgphzVlpCaMjw/pOURG1qId1Qf66PS24b3RHo75Y+qLiat+jfpLyB0klxBAD1BLivr3haDVe36Nxy7p1o4M5j7MM5s8XbJl82GBW1kf7rGI12fDpQrZ9o37P/xnDGu86Mfkq00ZTAJ8bc/m/tDXQhWpRynXfPlXv7357whuehjqKllM6kNqpozroUXZCAso2qtCYlWyGKVQrlemovBQeD85fH0B/rvXtzXNkKb65FxY4Xfo2ZmFGMmryejQn+uMG5TFEMJ5LMeE1YGOLpNvlNcwsiWt5TkSfXz1asQS9+zgMBKzCvdclMDGjBL7xqSa0uN2kqmjGU/TjzCNz75HVJvqMZM8JG6u",
